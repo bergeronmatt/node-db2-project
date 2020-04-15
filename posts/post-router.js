7ad98c06a8089766cfc6e6b2ee84b3ea57354f90
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-14 20:49:19 -0400

    added in the database points in the endpoints

--- posts/post-router.js
+++ posts/post-router.js
@@ -23,9 +23,9 @@ const router = express.Router();
 // Read the entire database
 router.get('/', (req, res) => {
     db.select('*') //read the entire database
-        .from('DB') //from accounts wihin the accounts.js
-        .then( VAR => { //create an account variable
-            res.status(200).json({data: VAR}) //use json to render the account data in the browser
+        .from('cars') //from accounts wihin the accounts.js
+        .then( cars => { //create an account variable
+            res.status(200).json({data: cars}) //use json to render the account data in the browser
         })
         .catch(err => { //error code if the data could not be found
             res.status(500).json({error: 'there was an error retrieving the data', err})
@@ -34,12 +34,12 @@ router.get('/', (req, res) => {
 
 // Access the datasets by the id
 router.get('/:id', (req, res) => {
-    db('DB') //read the database
+    db('cars') //read the database
         .where({ //set up the conditions to finding the information
             id: req.params.id //access the requested dataset through the id parameter
         })
-        .then( VAR => {//take the accessed dataset and render it to the browser via json
-            res.status(201).json({data: VAR}) //send success message and render the data
+        .then( cars => {//take the accessed dataset and render it to the browser via json
+            res.status(201).json({data: cars}) //send success message and render the data
         })
         .catch(err => { //catch statement in the event the data cannot be rendered
             res.status(500).json({error: err.message})
@@ -51,15 +51,15 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     const MYDBData = req.body //set up a variable to handle the data that's being pushed to the database
-    db('DB') //access the database
+    db('cars') //access the database
     .insert(MYDBData, 'id')//insert the data being presented and assign it an id
     .then(ids => { //then statement to being generating the id for the account posted
         const id = ids[0]; //setting up an id variable and then searching for a place in the array for it
-        db('DB') //accessing the accounts database again
+        db('cars') //accessing the accounts database again
             .where({id}) //checking the next available id in the chain
             .first()
-            .then( VARSINGLE => { //render the account to the browser with success message
-                res.status(201).json({data: VARSINGLE})
+            .then( car => { //render the account to the browser with success message
+                res.status(201).json({data: car})
             });
     })
     .catch(err => { //catch statment with error message in the event it cannot be saved to the database
@@ -72,7 +72,7 @@ router.post('/', (req, res) => {
 router.patch('/:id', (req, res) => {
     const changes = req.body; //set up variable to store the changes to the account information
     const {id} = req.params; //set up an id object to store and handle the parameters of the information being changed
-    db('DB') //access the accounts database in accounts.js
+    db('cars') //access the accounts database in accounts.js
         .where({id}) //stipulation clause searching for the id of the object being updated
         .update(changes) //update function to pass the changes variable
         .then(count => { //setting up an if/else statement to parse through data to find the specific id of the account being updated
@@ -90,7 +90,7 @@ router.patch('/:id', (req, res) => {
 //DELETE
 
 router.delete('/:id', (req, res) => {
-    db('DB')//access the accounts database
+    db('cars')//access the accounts database
     .where({id : req.params.id}) //find the specific account by its id
     .del() //delete the selected account
     .then(count => {
